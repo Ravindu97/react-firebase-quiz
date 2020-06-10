6236daf3b371e3b80e57b5a6c714f7034b70df0e
RavinduFernando <f.ravindu@gmail.com>
2020-06-10 18:27:41 +0530

    rework the started with the scrath

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { Component } from "react";
-import SignIn from "./Components/signIn";
-// import SignUp from "./Components/signup";
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import { AuthProvider } from "./Auth";
+import PrivateRoute from "./PrivateRoute";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <SignIn />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <div>
+          <PrivateRoute exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+};
+
 export default App;
---
+++ src/base.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyB_VxrWnWXt6RANpj9_ashIuU2wpGVh2Tc",
+  authDomain: "fir-quiz-b51f8.firebaseapp.com",
+  databaseURL: "https://fir-quiz-b51f8.firebaseio.com",
+  projectId: "fir-quiz-b51f8",
+  storageBucket: "fir-quiz-b51f8.appspot.com",
+  messagingSenderId: "625682568928",
+  appId: "1:625682568928:web:3c3587d92fb899e02aa285",
+  measurementId: "G-V6K6DJ35W8",
+});
+
+export default app;
